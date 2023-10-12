1451f9b0343e1e0bfaf6420d524f6683ac1de271
GeekySazzad <71886049+sazzadalam112@users.noreply.github.com>
2023-10-12 23:57:34 +0530

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react';
+import Header from './assets/components/Header';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+ <Header />
+ <div className='bg-slate-600 w-full h-screen'></div>
+  </>
+  )
+}
+
+export default App;
---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BeakerIcon } from '@heroicons/react/24/solid'
+
+const Header = () => {
+  return (
+    <div className=''>
+       <div>
+         <div>
+          <BeakerIcon />
+           Hello
+         </div>
+       </div>
+    </div>
+  )
+}
+
+export default Header;
